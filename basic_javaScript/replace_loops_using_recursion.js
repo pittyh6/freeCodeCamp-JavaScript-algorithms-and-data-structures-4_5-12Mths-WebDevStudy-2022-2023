fb80998fa83d73d59da9dc9f63331c61c82899a7
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-09-13 08:44:53 +1000

    replace loops using recursion

--- basic_javaScript/replace_loops_using_recursion.js
+++ basic_javaScript/replace_loops_using_recursion.js
@@ -2,10 +2,16 @@
 
 function sum(arr, n) {
     // Only change code below this line
-
+    if(n <= 0){
+        return n;
+    }else{
+        console.log("arr, n-1: " + arr,n-1)
+        console.log((arr, n-1) + arr[n-1])
+        return sum(arr, n-1) + arr[n-1];
+    }
     // Only change code above this line
 }
 
-sum([1], 0); // =0
-sum([2, 3, 4], 1); // =2
+//sum([1], 0); // =0
+//sum([2, 3, 4], 1); // =2
 sum([2, 3, 4, 5], 3); // =9
