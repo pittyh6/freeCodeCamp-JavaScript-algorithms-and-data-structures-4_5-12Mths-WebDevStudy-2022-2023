49758e14f70db6852d585eb84bb4777098ac883f
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-11 07:35:07 +1100

    basic data structure -  object for...in loop

---
+++ basic_data_structure/for_in.js
@@ -0,0 +1,59 @@
+/*
+    We've defined a function countOnline which accepts one argument (a users object). 
+    Use a for...in statement within this function to loop through the users object passed into the function 
+    and return the number of users whose online property is set to true. 
+    An example of a users object which could be passed to countOnline is shown below. 
+    Each user will have an online property with either a true or false value.
+*/
+
+const users = {
+    Alan: {
+        online: false
+    },
+    Jeff: {
+        online: true
+    },
+    Sarah: {
+        online: false
+    }
+}
+
+const usersTwo = {
+    Alan: {
+        online: true
+    },
+    Jeff: {
+        online: false
+    },
+    Sarah: {
+        online: true
+    }
+}
+const usersThree = {
+    Alan: {
+        online: false
+    },
+    Jeff: {
+        online: false
+    },
+    Sarah: {
+        online: false
+    }
+}
+
+function countOnline(usersObj) {
+    let count = 0;
+    for (let user in usersObj) {
+        if (usersObj[user].online === true) {
+            console.log("true...")
+            count++
+        } else {
+            console.log("false...")
+        }
+    }
+    return count;
+}
+
+console.log(countOnline(users));
+console.log(countOnline(usersTwo));
+console.log(countOnline(usersThree));
--- learnedBasicDataStructure.md
+++ learnedBasicDataStructure.md
@@ -209,7 +209,9 @@
   * Now we can add, modify, and remove keys from objects. But what if we just wanted to know if an object has a specific property? JavaScript provides us with two different ways to do this. One uses the hasOwnProperty() method and the other uses the in keyword. If we have an object users with a property of Alan, we could check for its presence in either of the following ways:
     * users.hasOwnProperty('Alan');
     * 'Alan' in users;
-
+# OBJECT for...in
+* Iterate Through the Keys of an Object with a for...in Statement
+  * 
 
 
 ![End Banner](/Documentation/botton-1200x350.gif)
