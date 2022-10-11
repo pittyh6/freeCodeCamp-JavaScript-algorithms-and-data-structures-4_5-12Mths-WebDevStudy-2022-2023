c042bb3526fa4583b395056d9d3388e3ad746aa3
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-12 07:55:19 +1100

    basic algorithm scripting - logest string done

--- basic_algorithm_scripting/longest_string_word.js
+++ basic_algorithm_scripting/longest_string_word.js
@@ -7,14 +7,28 @@
 
 function findLongestWordLength(str) {
     let division = str.split(" ")
-    let longest = division[0] 
-    for(let i = 1; i < division.length; i++){
-        if(longest.length < division[i].length){
+    let longest = division[0]
+    for (let i = 1; i < division.length; i++) {
+        if (longest.length < division[i].length) {
             longest = division[i]
         }
     }
     console.log("result: " + longest)
     return longest.length;
-  }
-  
-  findLongestWordLength("The quick brown fox jumped over the lazy dog");
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+
+/*
+function findLongestWordLength(s) {
+return s.split(' ')
+  .reduce(function(longest, word) {
+    return Math.max(longest, word.length)
+  }, 0);
+}
+
+function findLongestWordLength(str) {
+  return Math.max(...str.split(" ").map(word => word.length));
+}
+*/
