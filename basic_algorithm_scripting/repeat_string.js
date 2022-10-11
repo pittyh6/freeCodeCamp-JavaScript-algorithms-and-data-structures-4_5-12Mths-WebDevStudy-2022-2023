a41aeaa84a64d37ad9bfe55445a621ea6335350c
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-12 10:05:19 +1100

    basic algorithm scripting - tepeat a string repeat a string - start

---
+++ basic_algorithm_scripting/repeat_string.js
@@ -0,0 +1,6 @@
+/*
+Repeat a given string str (first argument) for num times (second argument). 
+Return an empty string if num is not a positive number. For the purpose of this challenge, 
+do not use the built-in .repeat() method.
+*/
+
