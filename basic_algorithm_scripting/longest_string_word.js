333c0fa07786c8e0ec658c0614ba239d6635c1b9
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-11 12:03:48 +1100

    basic algorithm scripting - logest string start

--- basic_algorithm_scripting/longest_string_word.js
+++ basic_algorithm_scripting/longest_string_word.js
@@ -1,3 +1,22 @@
 /*
-    
+    Find the Longest Word in a String
+    Return the length of the longest word in the provided sentence.
+
+    Your response should be a number.
 */
+
+function findLongestWordLength(str) {
+    console.log(str.split(" "))
+    let test = str.split(" ")
+    for(let i = 0; i < test.length; i++) {
+        console.log(test[i])
+        if(test[i] < test[i + 1]) {
+            console.log("small: " + test[i] + " " + test[i + 1])
+        }else{
+            console.log("big: " + test[i + 1] + " " + test[i])
+        }
+    }
+    return str.length;
+  }
+  
+  findLongestWordLength("The quick brown fox jumped over the lazy dog");
