aedf6a7053828ae696c237a50a9c1a186a4ff997
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-12 11:47:12 +1100

    basic algorithm scripting - tepeat a string repeat a string - done

--- basic_algorithm_scripting/repeat_string.js
+++ basic_algorithm_scripting/repeat_string.js
@@ -4,3 +4,39 @@ Return an empty string if num is not a positive number. For the purpose of this 
 do not use the built-in .repeat() method.
 */
 
+function repeatStringNumTimes(str, num) {
+    let rep = str;
+    if(num <= 0){
+        return ""
+    }else{
+        for(let i = 1; i < num; i++){
+            rep += str
+        }
+    }
+    console.log(rep)
+    return rep;   
+}
+repeatStringNumTimes("abc", 3);
+
+
+/*
+function repeatStringNumTimes(str, num) {
+  let accumulatedStr = "";
+  for (let i = 0; i < num; i++) {
+    accumulatedStr += str;
+  }
+  return accumulatedStr;
+}
+
+function repeatStringNumTimes(str, num) {
+  if (num < 1) {
+    return "";
+  } else {
+    return str + repeatStringNumTimes(str, num - 1);
+  }
+}
+
+function repeatStringNumTimes(str, num) {
+  return num > 0 ? str + repeatStringNumTimes(str, num - 1) : '';
+}
+*/
