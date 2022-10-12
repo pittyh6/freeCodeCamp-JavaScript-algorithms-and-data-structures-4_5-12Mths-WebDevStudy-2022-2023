ab117dda8b5dae6c032c210cb868138db66be6cc
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-12 17:42:47 +1100

    upte truncate js example solutions

--- basic_algorithm_scripting/truncate_string.js
+++ basic_algorithm_scripting/truncate_string.js
@@ -15,3 +15,10 @@ function truncateString(str, num) {
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+
+/*
+function truncateString(str, num) {
+  return str.length > num ? str.slice(0, num) + "..." : str;
+}
+*/
