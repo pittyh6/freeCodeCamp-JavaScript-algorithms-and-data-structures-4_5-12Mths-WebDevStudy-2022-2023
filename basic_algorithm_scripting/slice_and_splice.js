bf9e8be32b0fc303cd7ab7b90fd8a4d82ec51eae
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-16 10:13:16 +1100

    basic algorithm scripting - slice and splice - done

--- basic_algorithm_scripting/slice_and_splice.js
+++ basic_algorithm_scripting/slice_and_splice.js
@@ -10,18 +10,45 @@ Return the resulting array. The input arrays should remain the same after the fu
 */
 
 function frankenSplice(arr1, arr2, n) {
-    let result = arr1;
-    //n_new = arr2[n - 1]
-    let n_new = arr2.splice(n-1,n)
-    let n_rest = arr2.splice(n-1, arr2.length)
-
-    result.unshift(n_new);
-    result.push(n_rest)
-    console.log("final: " + result)
+    let secArr = [...arr2]
+    let restArr = secArr.splice(n);
+    let result = secArr;
+    for(let i = 0; i < arr1.length; i++) {
+        result.push(arr1[i]);
+    }
+    restArr.map(x => result.push(x))
+    console.log(result)
     return result;
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
 frankenSplice([1, 2, 3], [4, 5], 1) //[4, 1, 2, 3, 5].
 frankenSplice([1, 2], ["a", "b"], 1) //should return ["a", 1, 2, "b"]
-Failed:frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2)// should return ["head", "shoulders", "claw", "tentacle", "knees", "toes"].
+frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2)// should return ["head", "shoulders", "claw", "tentacle", "knees", "toes"].
+
+
+
+/*
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let localArray = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    localArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return localArray;
+}
+
+
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let localArr = arr2.slice();
+  localArr.splice(n, 0, ...arr1);
+  return localArr;
+}
+
+
+function frankenSplice(arr1, arr2, n) {
+  return [...arr2.slice(0, n), ...arr1, ...arr2.slice(n)];
+}
+*/
