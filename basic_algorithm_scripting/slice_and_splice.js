3716c03913cfb0912363615df5ea5f11ff028442
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-15 19:33:19 +1100

    basic algorithm scripting - slice and splice - progress

--- basic_algorithm_scripting/slice_and_splice.js
+++ basic_algorithm_scripting/slice_and_splice.js
@@ -10,7 +10,18 @@ Return the resulting array. The input arrays should remain the same after the fu
 */
 
 function frankenSplice(arr1, arr2, n) {
-    return arr2;
+    let result = arr1;
+    //n_new = arr2[n - 1]
+    let n_new = arr2.splice(n-1,n)
+    let n_rest = arr2.splice(n-1, arr2.length)
+
+    result.unshift(n_new);
+    result.push(n_rest)
+    console.log("final: " + result)
+    return result;
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
+frankenSplice([1, 2, 3], [4, 5], 1) //[4, 1, 2, 3, 5].
+frankenSplice([1, 2], ["a", "b"], 1) //should return ["a", 1, 2, "b"]
+Failed:frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2)// should return ["head", "shoulders", "claw", "tentacle", "knees", "toes"].
