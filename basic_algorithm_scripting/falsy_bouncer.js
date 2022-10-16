756e0722ed26c49cecbfb69b85eca38a317d7e35
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-16 11:41:46 +1100

    basic algorithm scripting - falsy bouncer - done

--- basic_algorithm_scripting/falsy_bouncer.js
+++ basic_algorithm_scripting/falsy_bouncer.js
@@ -9,7 +9,23 @@ Hint: Try converting each value to a Boolean.
 
 
 function bouncer(arr) {
-    return arr;
+    let new_arr = []
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i]){
+            new_arr.push(arr[i]) //We use the if statement to check if the current element is truthy 2.8k or falsy 4.5k
+        }
+    }
+    console.log(new_arr)
+    return new_arr;
 }
 
-bouncer([7, "ate", "", false, 9]);
+bouncer([7, "ate", "",false, 9]); //[7, "ate", 9].
+bouncer(["a", "b", "c"]) //should return ["a", "b", "c"].
+bouncer([false, null, 0, NaN, undefined, ""])// should return []
+/*
+function bouncer(arr) {
+  return arr.filter(Boolean);
+}
+The Array.prototype.filter method expects a function that returns a Boolean value which takes a single argument and returns true for truthy 2.8k value or false for falsy 4.5k value. Hence we pass the built-in Boolean function.
+
+*/
