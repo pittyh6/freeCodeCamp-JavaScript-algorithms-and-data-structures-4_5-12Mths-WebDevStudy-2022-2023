ff3ce474dd26105e76fd4f6603827220a7f28c00
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-11-02 09:18:41 +1100

    diff two arrays

---
+++ intermediate_algorithm_scripting/diff_two_arrays.js
@@ -0,0 +1,64 @@
+/*
+Diff Two Arrays
+Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+In other words, return the symmetric difference of the two arrays.
+
+Note: You can return the array with its elements in any order.
+*/
+
+function diffArray(arr1, arr2) {
+    const newArr = arr1.concat(arr2).sort().filter(elem => !arr1.includes(elem) || !arr2.includes(elem));
+
+    console.log(newArr);
+    return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"])
+
+/*
+function diffArray(arr1, arr2) {
+  const newArr = [];
+
+  function onlyInFirst(first, second) {
+    // Looping through an array to find elements that don't exist in another array
+    for (let i = 0; i < first.length; i++) {
+      if (second.indexOf(first[i]) === -1) {
+        // Pushing the elements unique to first to newArr
+        newArr.push(first[i]);
+      }
+    }
+  }
+
+  onlyInFirst(arr1, arr2);
+  onlyInFirst(arr2, arr1);
+
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+
+
+function diffArray(arr1, arr2) {
+  return arr1
+    .concat(arr2)
+    .filter(item => !arr1.includes(item) || !arr2.includes(item));
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+
+
+
+
+function diffArray(arr1, arr2) {
+  return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+
+  function diff(a, b) {
+    return a.filter(item => b.indexOf(item) === -1);
+  }
+}
+*/
