f6118b2b5f750a6264f5fbf31084c294a321fc77
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-11-01 13:16:52 +1100

    sum all numbers in a range

---
+++ intermediate_algorithm_scripting/sum_all_numbers_range.js
@@ -0,0 +1,77 @@
+/*
+We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. 
+The lowest number will not always come first.
+
+For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
+*/
+
+function sumAll(arr) {
+    const max = Math.max(...arr.values())
+    const min = Math.min(...arr.values())
+    let sum = min;
+    //console.log(min)
+    //console.log(max)
+    for (let i = (min + 1); i <= max; i++) {
+        sum += i
+    }
+    console.log(sum)
+    return sum;
+}
+
+sumAll([1, 4]);
+sumAll([4, 1]);
+sumAll([5, 10])
+sumAll([10, 5])
+
+
+
+/*
+function sumAll(arr) {
+  let max = Math.max(arr[0], arr[1]);
+  let min = Math.min(arr[0], arr[1]);
+  let sumBetween = 0;
+  for (let i = min; i <= max; i++) {
+    sumBetween += i;
+  }
+  return sumBetween;
+}
+sumAll([1, 4]);
+
+
+
+const sumAll = arr => {
+  // Buckle up everything to one!
+  const startNum = arr[0];
+  const endNum = arr[1];
+
+  // Get the count of numbers between the two numbers by subtracting them and add 1 to the absolute value.
+  // ex. There are |1-4| + 1 = 4, (1, 2, 3, 4), 4 numbers between 1 and 4.
+  const numCount = Math.abs(startNum - endNum) + 1;
+
+  // Using Arithmetic Progression summing formula
+  const sum = ((startNum + endNum) * numCount) / 2;
+  return sum;
+};
+
+
+function sumAll(arr) {
+  let sumBetween = 0;
+  for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
+    sumBetween += i;
+  }
+  return sumBetween;
+}
+sumAll([1, 4]);
+
+
+
+
+function sumAll(arr) {
+  const [first, last] = [...arr].sort((a, b) => a - b);
+  return first !== last
+    ? first + sumAll([first + 1, last])
+    : first;
+}
+
+sumAll([1, 4]);
+*/
