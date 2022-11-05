8cca3345b397f846455954d03e1524f707fe2453
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-11-05 20:15:20 +1100

    spinal tap case done

--- intermediate_algorithm_scripting/spinal_tap_case.js
+++ intermediate_algorithm_scripting/spinal_tap_case.js
@@ -5,8 +5,17 @@ Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-da
 
 
 function spinalCase(str) {
-    console.log(str.split(/(?=[A-Z])/).join("-").split(/[_\s]/).join("").toLowerCase()); 
-    return str.split(" ").join("-").toLowerCase();
+
+    let newStr = str.split(/[_\s]/).join("-").split(/(?=[A-Z])/).join("-").toLowerCase();
+
+    for (let i = 0; i < newStr.length; i++) {
+        if (newStr.includes("--")) {
+            newStr = newStr.replace("--", "-")
+        }
+    }
+
+    console.log(newStr)
+    return newStr
 }
 
 spinalCase('This Is Spinal Tap');
@@ -15,5 +24,37 @@ spinalCase("The_Andy_Griffith_Show")
 spinalCase("Teletubbies say Eh-oh")
 
 /*
-1) 
+function spinalCase(str) {
+  // Create a variable for the white space and underscores.
+  var regex = /\s+|_+/g;
+
+  // Replace low-upper case to low-space-uppercase
+  str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+  // Replace space and underscore with -
+  return str.replace(regex, "-").toLowerCase();
+}
+
+
+
+
+function spinalCase(str) {
+  // Replace low-upper case to low-space-uppercase
+  str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+  // Split on whitespace and underscores and join with dash
+  return str
+    .toLowerCase()
+    .split(/(?:_| )+/)
+    .join("-");
+}
+
+
+
+function spinalCase(str) {
+  return str
+    .split(/\s|_|(?=[A-Z])/)
+    .join("-")
+    .toLowerCase();
+}
+
 */
