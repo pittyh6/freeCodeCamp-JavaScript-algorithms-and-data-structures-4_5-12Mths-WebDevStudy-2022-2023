f02761855d9d3e40fa87d23b648b658d0dd4a4e0
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-11-06 12:15:43 +1100

    pig latin done

--- intermediate_algorithm_scripting/pig_latin.js
+++ intermediate_algorithm_scripting/pig_latin.js
@@ -11,20 +11,60 @@ Translate the provided string to Pig Latin. Input strings are guaranteed to be E
 
 */
 function translatePigLatin(str) {
-
+    let myRegex = /[aeiou]/gi
     let firstLetter
-    if(str.charAt(0)== 'a' || str.charAt(0)== 'e' || str.charAt(0)== 'i' || str.charAt(0)== 'o' || str.charAt(0)== 'u'){
+    if (str.charAt(0).match(myRegex)) {
         str += "way"
-    }else{
-        firstLetter = str.slice(0,1)
-        str = str.slice(1, str.length)+firstLetter + "ay"
+    } else if (str.match(myRegex) === null) {
+        str += "ay"
+    } else {
+        let vowelIndice = str.indexOf(str.match(myRegex)[0])
+        console.log(vowelIndice)
+        str = str.substring(vowelIndice) + str.substring(0, vowelIndice) + "ay"
     }
     console.log(str)
-    return str;
+    return str
 }
+
+
 
 translatePigLatin("consonant");
 translatePigLatin("california")
 translatePigLatin("paragraphs")
 translatePigLatin("algorithm")
 translatePigLatin("rhythm")
+translatePigLatin("schwartz")
+
+
+/*
+function translatePigLatin(str) {
+  let consonantRegex = /^[^aeiou]+/;
+  let myConsonants = str.match(consonantRegex);
+  return myConsonants !== null
+    ? str
+        .replace(consonantRegex, "")
+        .concat(myConsonants)
+        .concat("ay")
+    : str.concat("way");
+}
+
+
+
+
+function translatePigLatin(str) {
+  if (str.match(/^[aeiou]/)) return str + "way";
+
+  const consonantCluster = str.match(/^[^aeiou]+/)[0];
+  return str.substring(consonantCluster.length) + consonantCluster + "ay";
+}
+
+
+
+function translatePigLatin(str, charPos = 0) {
+  return ['a', 'e', 'i', 'o', 'u'].includes(str[0])
+    ? str + (charPos === 0 ? 'way' : 'ay')
+    : charPos === str.length
+      ? str + 'ay'
+      : translatePigLatin(str.slice(1) + str[0], charPos + 1);
+}
+*/
