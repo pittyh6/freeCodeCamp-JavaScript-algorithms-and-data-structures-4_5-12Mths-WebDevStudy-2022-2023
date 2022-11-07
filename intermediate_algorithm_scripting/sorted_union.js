b84bf2e46d9bb6d9d24b6f964dd1044ad3045e41
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-11-07 11:05:11 +1100

    sorted union styarted

--- intermediate_algorithm_scripting/sorted_union.js
+++ intermediate_algorithm_scripting/sorted_union.js
@@ -10,7 +10,22 @@ Check the assertion tests for examples.
 */
 
 function uniteUnique(arr) {
+    console.log();
+    let newArr = []
+    arr.filter(elem => {
+        if(!newArr.includes(elem)){
+            console.log("not included")
+            newArr.push(elem)
+        }else{
+            console.log("included")
+        }
+    })
+    console.log(newArr)
     return arr;
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+// uniteUnique([1, 2, 3], [5, 2, 1])
+// uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])
+// uniteUnique([1, 3, 2], [5, 4], [5, 6])
+// uniteUnique([1, 3, 2, 3], [5, 2, 1, 4], [2, 1])
