84512b0ee896195be04ac38a43b2077b46078617
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-11-11 10:10:35 +1100

    rules done

--- intermediate_algorithm_scripting/sum_primes.js
+++ intermediate_algorithm_scripting/sum_primes.js
@@ -7,7 +7,31 @@ Rewrite sumPrimes so it returns the sum of all prime numbers that are less than 
 
 
 function sumPrimes(num) {
-    let sum = 0;
+    function isPrime(num) {
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+          if (num % i == 0)
+            return false;
+        }
+        return true;
+      }
+    
+      // Check all numbers for primality
+      let sum = 0;
+      for (let i = 2; i <= num; i++) {
+        if (isPrime(i))
+          sum += i;
+      }
+      console.log(sum);
+      return sum;
+}
+
+sumPrimes(10);
+sumPrimes(977)
+
+
+
+/*
+let sum = 0;
     for (let i = 2; i <= num; i++) {
         if (i > 1 && i <= 3) {
             console.log("Prime: " + i)
@@ -31,7 +55,67 @@ function sumPrimes(num) {
     }
     console.log("sum: " + sum)
     return sum;
+*/
+
+
+
+/*
+function sumPrimes(num) {
+  // Helper function to check primality
+  function isPrime(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i == 0)
+        return false;
+    }
+    return true;
+  }
+
+  // Check all numbers for primality
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i))
+      sum += i;
+  }
+  return sum;
 }
 
-sumPrimes(10);
-sumPrimes(977)
+
+
+
+
+
+function sumPrimes(num) {
+  // Prime number sieve
+  let isPrime = Array(num + 1).fill(true);
+  // 0 and 1 are not prime
+  isPrime[0] = false;
+  isPrime[1] = false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (isPrime[i]) {
+      // i has not been marked false -- it is prime
+      for (let j = i * i; j <= num; j += i)
+        isPrime[j] = false;
+    }
+  }
+
+  // Sum all values still marked prime
+  return isPrime.reduce(
+    (sum, prime, index) => prime ? sum + index : sum, 0
+  );
+}
+
+
+
+
+
+
+function sumPrimes(num) {
+  // Check all numbers for primality
+  let primes = [];
+  for (let i = 2; i <= num; i++) {
+    if (primes.every((prime) => i % prime !== 0))
+      primes.push(i);
+  }
+  return primes.reduce((sum, prime) => sum + prime, 0);
+}
+*/
