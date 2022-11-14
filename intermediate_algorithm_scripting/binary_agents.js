d59a2d5846476e34432d15a334300362fb31f71e
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-11-14 12:24:38 +1100

    copy binary agents

---
+++ intermediate_algorithm_scripting/binary_agents.js
@@ -0,0 +1,21 @@
+/*
+Binary Agents
+Return an English translated sentence of the passed binary string.
+
+The binary string will be space separated.
+*/
+
+function binaryAgent(str) {
+    let binaryData = str.split(" ")
+    let decipheredText = ""
+
+    binaryData.forEach(function(binaryDataPoint) {
+        let characterCode = parseInt(binaryDataPoint, 2) 
+        let decipheredLetter = String.fromCharCode(characterCode)
+        decipheredText += decipheredLetter
+    })
+    console.log(decipheredText)
+    return decipheredText;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
