8944bd68700539784ef16c48f646e505add64d29
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-11-16 09:33:42 +1100

    arguments optional done

--- intermediate_algorithm_scripting/arguments_optional.js
+++ intermediate_algorithm_scripting/arguments_optional.js
@@ -15,100 +15,52 @@ If either argument isn't a valid number, return undefined.
 
 
 function addTogether() {
-   
+    let firstArgument = arguments[0];
+    let sum = 0
     if (arguments.length > 1) {
-        if (typeof arguments[0] == 'number' && typeof arguments[1] == 'number') {
-            let sum = arguments[0] + arguments[1]
-            console.log(sum)
-            return sum
-        } else if (typeof arguments[0] !== 'number' && typeof arguments[1] !== 'number') {
-            argumentCheck(arguments)
-        }
-    } else if (arguments.length = 1) {
-        if (typeof arguments[0] !== 'number') {
-            argumentCheck(arguments)
+        let secondArgument = arguments[1];
+        return checkArgumentsAndAddIfValid(firstArgument, secondArgument)
+    } else if (arguments.length == 1) {
+        if (argumentCheck(firstArgument) == undefined) {
+            return undefined
+        } else {
+            return function (secondArgument) {
+                return checkArgumentsAndAddIfValid(firstArgument, secondArgument)
+            }
         }
     }
 
 }
 
-function argumentCheck(argument){
-    if(typeof argument === 'number'){
+function argumentCheck(argument) {
+    if (typeof argument === 'number') {
         return argument
-    }else{
+    } else {
         return undefined;
+    }
+}
+
+function checkArgumentsAndAddIfValid(first, second) {
+    if (argumentCheck(first) != undefined && argumentCheck(second) != undefined) {
+        sum = first + second
+        console.log(sum)
+        return sum
+    } else {
+        console.log("undefined")
+        return undefined
     }
 }
 
 
 addTogether(2, 3);
-addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-//addTogether(5)(7)
+//addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+addTogether(23, 30)
+//addTogether(2, "3")
+//addTogether("2", 3)
+//addTogether(5, undefined)
+addTogether(5)(7)
+//addTogether(2)([3])
 
 
 
 
-
-/*
-if (isNaN(typeof arguments[0] == 'number' && typeof arguments[1] == 'number') ){
-        if (arguments.length = 2) {
-            let sum = arguments[0] + arguments[1]
-            console.log(sum)
-            //return sum
-        } else if(arguments.length = 1){
-            console.log("only one")
-        }
-    }else if (isNaN(arguments[0]) !== false || isNaN(arguments[1] !== false)) {
-        console.log("undefined")
-        return undefined
-    }
-
-
-
-    console.log(arguments.length);
-    const first = arguments[0];
-    console.log(first)
-    if (arguments.length = 2) {
-        if (typeof arguments[0] == 'number' && typeof arguments[1] == 'number') {
-            let sum = arguments[0] + arguments[1]
-            console.log(sum)
-            //return sum
-        } else if (isNaN(arguments[0]) !== false || isNaN(arguments[1] !== false)) {
-            console.log("undefined")
-            return undefined
-        }
-    } else if (arguments.length = 1) {
-        
-    }
-
-
-
-
-function addTogether() {
-   
-    if (arguments.length > 1) {
-        if (typeof arguments[0] == 'number' && typeof arguments[1] == 'number') {
-            let sum = arguments[0] + arguments[1]
-            console.log(sum)
-            return sum
-        } else if (typeof arguments[0] !== 'number' && typeof arguments[1] !== 'number') {
-            argumentCheck(arguments)
-        }
-    } else if (arguments.length = 1) {
-        if (typeof arguments[0] !== 'number') {
-            argumentCheck(arguments)
-        }
-    }
-
-}
-
-function argumentCheck(argument){
-    if(typeof argument === 'number'){
-        return argument
-    }else{
-        return undefined;
-    }
-}
-
-
-*/
