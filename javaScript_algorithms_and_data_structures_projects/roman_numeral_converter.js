b737b3533ca705b939bafdf02150bc4fe69339a9
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-12-01 12:10:01 +1100

    roman copy

--- javaScript_algorithms_and_data_structures_projects/roman_numeral_converter.js
+++ javaScript_algorithms_and_data_structures_projects/roman_numeral_converter.js
@@ -20,7 +20,31 @@ All roman numerals answers should be provided in upper-case.
 */
 
 function convertToRoman(num) {
-    return num;
+    let romanNumerals = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
+    }
+    let roman = ""
+    for(let key in romanNumerals) {
+        while(num >= romanNumerals[key]) {
+            roman += key
+            console.log("romanNumerals[key]: ", romanNumerals[key])
+            num -= romanNumerals[key]
+        }
+    }
+    console.log("roman: ", roman)
+    return roman;
 }
 
-convertToRoman(36);
+convertToRoman(30);
